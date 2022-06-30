ba9e718c73bbb7ad149e7511068fa138aaa15109
sadhon <roysadhon16@gmail.com>
2022-07-01 00:09:15 +0600

    changes

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const element=document.getElementById("ul");
+const b2=document.getElementById("b2");
+const b1=document.getElementById("b1");
+const b3=document.getElementById("b3");
+const p=document.getElementById("p");
+function show(){
+    element.style.display='block';
+    b3.style.display='none';
+    b1.style.display='none';
+    b2.style.backgroundColor='red';
+    b2.style.height='25px';
+    b2.style.borderRadius='30px';
+    p.style.display="block";
+    
+}
+
+
